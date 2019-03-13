d8afee35b1384eed4d8cb37764d90953f75e885e
Prashant rawal <Prashantrawal619@gmail.com>
2019-03-13 10:16:53 +0530

    context api learned in cookbook

--- advanced-components/components-cookbook/src/components/theme/src/App.js
+++ advanced-components/components-cookbook/src/components/theme/src/App.js
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import {ThemeContext, themes} from './theme';
-import './App.css';
-import Header from './Header';
+import { ThemeContext, themes } from "./theme";
+import "./App.css";
+import Header from "./Header";
 
 class App extends Component {
-  state = {theme: themes.dark};
+  state = { theme: themes.dark };
 
   changeTheme = evt => {
     this.setState(state => ({
       theme: state.theme === themes.dark ? themes.light : themes.dark
     }));
   };
-
+  //when we provide the context we also import the values (here themes for state management)
+  //we just wrap the component in which we want this context data to live (here Header is wrapped in ThemeContext.Provider  )
   render() {
     return (
       <div className="App">
--- advanced-components/components-cookbook/src/components/theme/src/Body.js
+++ advanced-components/components-cookbook/src/components/theme/src/Body.js
@@ -1,17 +1,17 @@
-import React from 'react';
-
-import {ThemeContext} from './theme';
-import {UserContext} from './user';
-
+import React from "react";
+//using multiple contexts
+import { ThemeContext } from "./theme";
+import { UserContext } from "./user";
+//we just have to wrap the element in the context Consumer component then we can easily use that value
 export const Body = props => (
   <ThemeContext.Consumer>
     {theme => (
       <header
         className="App-header"
-        style={{backgroundColor: theme.background}}
+        style={{ backgroundColor: theme.background }}
       >
         <UserContext.Consumer>
-          <h1>{user => (user ? 'Welcome back' : 'Welcome')}</h1>
+          <h1>{user => (user ? "Welcome back" : "Welcome")}</h1>
         </UserContext.Consumer>
       </header>
     )}
