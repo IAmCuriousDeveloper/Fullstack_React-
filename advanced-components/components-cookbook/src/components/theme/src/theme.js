00f6326b21a1ed8ea91413040eef1dd6a4bf3459
Prashant rawal <Prashantrawal619@gmail.com>
2019-03-14 10:01:42 +0530

    added form section

--- advanced-components/components-cookbook/src/components/theme/src/theme.js
+++ advanced-components/components-cookbook/src/components/theme/src/theme.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export const themes = {
   light: {
-    foreground: '#222222',
-    background: '#e9e9e9'
+    foreground: "#222222",
+    background: "#e9e9e9"
   },
   dark: {
-    foreground: '#fff',
-    background: '#222222'
+    foreground: "#fff",
+    background: "#222222"
   }
 };
 
 export const ThemeContext = React.createContext(themes.dark);
+//while creating a conext default thing is passed
